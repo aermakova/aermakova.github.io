a709ffb206bcda8b1f46ee828348b3b13d29ac0d
aermakova <35726918+aermakova@users.noreply.github.com>
2018-03-15 15:40:52 +0700

    rsk commit

---
+++ portfolio/rsk/js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+ 
+    $('.nav_title').click(function(){
+        $(this).toggleClass('active');
+        $('nav').slideToggle();
+    });
+
+    $('.info').each(function(){
+		var $TabBlock = $('.info_desc_client div', this);
+		var $TabBtn = $('.info_buttons li', this);
+ 
+        $TabBtn.click(function(){
+            $TabBlock.eq($(this).index()).addClass('active').siblings().removeClass('active');
+            $(this).addClass('active').siblings().removeClass('active');
+        });
+	});
+});
