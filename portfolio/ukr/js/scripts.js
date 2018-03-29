4031b38e5d750e3b8d5f363e30c28b12081ed305
aermakova <35726918+aermakova@users.noreply.github.com>
2018-03-29 13:19:43 +0700

    ukr2 commit

--- portfolio/ukr/js/scripts.js
+++ portfolio/ukr/js/scripts.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 
- // castom select
-  var mySettings = {
+// castom select language for a start page
+var mySettings = {
         key : true,
         dropBlock : $('.drop'),
         virtualSelect : $('.select-wrap')
     }
-    
     mySettings.virtualSelect.click(function(){
         if( mySettings.dropBlock.is(':hidden') ) {
             mySettings.dropBlock.slideDown();
@@ -43,7 +42,50 @@ $(document).ready(function(){
         }   
     });
 
-    // language for a start page
-    $('.language .drop li').trigger('click');
+// select phone for a start page
+    $('.select .drop li').trigger('click');
+
+    var mySettingsPhone = {
+        key : true,
+        dropPhone : $('.drop__phone'),
+        virtualSelectPhone : $('.phone-wrap')
+    }
+    mySettingsPhone.virtualSelectPhone.click(function(){
+        if( mySettingsPhone.dropPhone.is(':hidden') ) {
+            mySettingsPhone.dropPhone.slideDown();
+            $(this).addClass('active');
+        } 
+        else {
+            mySettingsPhone.dropPhone.slideUp();
+            $(this).delay(100).queue(function(nextJ){
+                $(this).removeClass("active");
+                nextJ();
+            });
+        }
+        return false;
+    }); 
+    mySettingsPhone.dropPhone.find('li').click(function(){
+        var selectResult = $(this).html();
+        $(this).parent().parent().find('input').val(selectResult);
+        mySettingsPhone.dropPhone.slideUp();             
+        mySettingsPhone.virtualSelectPhone.delay(100).queue(function(nextJs){
+            $(this).removeClass("active");
+            nextJs();
+        }).html(selectResult);
+        return false;
+    }); 
+    $(document).click(function(event) {
+        if(mySettingsPhone.key && !$(event.target).closest(mySettingsPhone.dropPhone).length){
+            mySettingsPhone.dropPhone.slideUp();
+            mySettingsPhone.virtualSelectPhone.delay(100).queue(function(nextJs){
+                $(this).removeClass("active");
+                nextJs();
+            });
+            mySettingsPhone.key = true; 
+            return;
+        }   
+    });
+    // select phone for a start page
+    $('.select__phone .drop__phone li').trigger('click');
 
 });
