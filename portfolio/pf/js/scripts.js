11397f37572227872c0639998815f0869fe6b5b3
aermakova <35726918+aermakova@users.noreply.github.com>
2018-05-30 18:53:14 +0300

    2

--- portfolio/pf/js/scripts.js
+++ portfolio/pf/js/scripts.js
@@ -1,24 +1,20 @@
 $(function(){
 	
-	$('header a, .main_button button').click(function(){
-        var link = $(this).attr('href');
-        var coordinats = $(link).offset().top;
-        $('html, body').animate({scrollTop:coordinats}, 1000);
-        return false;
-    });
+	// $('header a, .main_button button').click(function(){
+ //        var link = $(this).attr('href');
+ //        var coordinats = $(link).offset().top;
+ //        $('html, body').animate({scrollTop:coordinats}, 1000);
+ //        return false;
+ //    });
 
-    new WOW().init();
+ //    new WOW().init();
 
     $('#layerslider').layerSlider({
         skinsPath : 'layerslider/skins/',
 		skin : 'fullwidth',
-        type: 'fullsize',
-        fullSizeMode: 'hero',
-        allowFullscreen: false,
-        maxRatio: 1,
-        hideUnder: 567,
+        responsive : true,
         showCircleTimer: false,
-        height: 720
+        autoStart: true
 	});
 });
 
