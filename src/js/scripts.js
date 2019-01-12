4a724fc2e2ca89a1c7e64d5a39798eea22698b18
Angelina Yermakova <ermakova.lina.lina@gmail.com>
2019-01-12 05:26:18 +0200

    add post css

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -31,4 +31,21 @@ $(function(){
         $('html, body').animate({scrollTop:coordinats}, 1000);
         return false;
     });
+
+    // window.addEventListener('scroll', function(event) {
+    //     let depth, layer, layers, movement, topDistance, translate3d, _i, _len;
+    //     topDistance = this.pageYOffset;
+    //     layers = document.querySelectorAll("[data-type='parallax']");
+    //     for (_i = 0, _len = layers.length; _i < _len; _i++) {
+    //         layer = layers[_i];
+    //         depth = layer.getAttribute('data-depth');
+    //         movement = -(topDistance * depth);
+    //         translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+    //         layer.style['-webkit-transform'] = translate3d;
+    //         layer.style['-moz-transform'] = translate3d;
+    //         layer.style['-ms-transform'] = translate3d;
+    //         layer.style['-o-transform'] = translate3d;
+    //         layer.style.transform = translate3d;
+    //     }
+    // });
 });
