a1db7323eb7f89850d23cfb9e362a2d8046da52c
Angelina Yermakova <ermakova.lina.lina@gmail.com>
2019-01-12 08:01:52 +0200

    add styles

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,6 @@
-$(window).scroll(function(){
-    $('.main_content').css({'top': -$(window).scrollTop()/3});
-});
+// $(window).scroll(function(){
+//     $('.main_content').css({'top': -$(window).scrollTop()/3});
+// });
 // $(document).on('mousemove', function (e) {
 //     $('.first').css({
 //         left: -e.pageX / 10 + 100,
@@ -32,20 +32,20 @@ $(function(){
         return false;
     });
 
-    // window.addEventListener('scroll', function(event) {
-    //     let depth, layer, layers, movement, topDistance, translate3d, _i, _len;
-    //     topDistance = this.pageYOffset;
-    //     layers = document.querySelectorAll("[data-type='parallax']");
-    //     for (_i = 0, _len = layers.length; _i < _len; _i++) {
-    //         layer = layers[_i];
-    //         depth = layer.getAttribute('data-depth');
-    //         movement = -(topDistance * depth);
-    //         translate3d = 'translate3d(0, ' + movement + 'px, 0)';
-    //         layer.style['-webkit-transform'] = translate3d;
-    //         layer.style['-moz-transform'] = translate3d;
-    //         layer.style['-ms-transform'] = translate3d;
-    //         layer.style['-o-transform'] = translate3d;
-    //         layer.style.transform = translate3d;
-    //     }
-    // });
+    window.addEventListener('scroll', function(event) {
+        let depth, layer, layers, movement, topDistance, translate3d, _i, _len;
+        topDistance = this.pageYOffset;
+        layers = document.querySelectorAll("[data-type='parallax']");
+        for (_i = 0, _len = layers.length; _i < _len; _i++) {
+            layer = layers[_i];
+            depth = layer.getAttribute('data-depth');
+            movement = -(topDistance * depth);
+            translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+            layer.style['-webkit-transform'] = translate3d;
+            layer.style['-moz-transform'] = translate3d;
+            layer.style['-ms-transform'] = translate3d;
+            layer.style['-o-transform'] = translate3d;
+            layer.style.transform = translate3d;
+        }
+    });
 });
