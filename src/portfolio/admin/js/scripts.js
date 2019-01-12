13826d489d50865dfc14c157241b33e99eb55fe9
Angelina Yermakova <ermakova.lina.lina@gmail.com>
2019-01-12 04:52:37 +0200

    delete portfolio folder

--- src/portfolio/admin/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $('.sidebar_button i').click(function(){
-        $('div.sidebar').toggleClass('all_panel');
-    });
-    $('.top_block li.wallet').click(function(){
-        $('.wallet_dropdown p').toggleClass('active');
-    });
-});
